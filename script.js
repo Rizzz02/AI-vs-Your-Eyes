0c8731f6fe756dc5bec7b623d667501b25cd6294
Rizzz <155726749+Rizzz02@users.noreply.github.com>
2026-05-07 09:02:00 +0700

    script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ uploadInput.addEventListener("change", async function () {
     formData.append("image", file);
 
     try {
-        const res = await fetch("https://your-app.onrender.com/upload", {
+        const res = await fetch("https://ai-vs-your-eyes.onrender.com/upload", {
             method: "POST",
             body: formData
         });
@@ -31,7 +31,7 @@ uploadInput.addEventListener("change", async function () {
         console.log("Backend:", data);
 
         // LOAD AI RESULT
-        aiImage.src = "https://your-app.onrender.com/upload" + new Date().getTime();
+        aiImage.src = "https://ai-vs-your-eyes.onrender.com/result?t=" + new Date().getTime();
 
     } catch (err) {
         console.error(err);
